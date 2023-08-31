325787903f51ed008450d31eb459eac514a5d754
Andrés Conde <andres.conde@innocv.com>
2023-08-31 17:25:20 +0200

    mocks updated

--- shared/src/commonMain/kotlin/com/example/stories/data/domain/mocks/Mocks.kt
+++ shared/src/commonMain/kotlin/com/example/stories/data/domain/mocks/Mocks.kt
@@ -18,7 +18,7 @@ class Mocks {
         History(
             id = id++,
             title = "Viaje al monte Bromo",
-            dateRange = LocalDate.now().add(monthNumber = -2).toRange(),
+            dateRange = LocalDate(2023, 5, 21).toRange(),
             elements = listOf(
                 Element.Image(id = id++, imageResource = "https://harindabama.files.wordpress.com/2012/10/bromo11.jpg"),
                 Element.Text(id = id++, text = "Monté a caballo en el mar de arena que rodea al volcán activo Bromo, en el este de Java."),
@@ -36,7 +36,7 @@ class Mocks {
         History(
             id = id++,
             title = "Submarinismo en el USS Liberty",
-            dateRange = LocalDate.now().add(monthNumber = -1).toRange(),
+            dateRange = LocalDate(2023, 7, 12).toRange(),
             elements = listOf(
                 Element.Text(id = id++, text = "Hice submarinismo dentro del USS Liberty, un barco estadounidense hundido en el noreste de Bali derante la Segunda Gerra Mundial por un submarino japonés."),
                 Element.Image(id = id++, imageResource = "https://media.tacdn.com/media/attractions-splice-spp-674x446/07/95/10/33.jpg"),
@@ -45,7 +45,7 @@ class Mocks {
         History(
             id = id++,
             title = "Visita a Kuala Lumpur",
-            dateRange = LocalDate.now().add(dayOfMonth = -7) range LocalDate.now(),
+            dateRange = LocalDate(2023, 8, 5) range LocalDate.now(),
             elements = listOf(
                 Element.Text(id = id++, text = "Estuve una semana en Kuala Lumpur, la capital de Malasia."),
                 Element.Image(id = id++, imageResource = "https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg"),
