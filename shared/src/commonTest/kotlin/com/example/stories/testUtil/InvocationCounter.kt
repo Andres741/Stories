056f80c47e090dba9285b2cc3eb22a6c0fbc5516
andres <aconderodriguez272@gmail.com>
2025-08-19 17:30:59 +0200

    add helper functions to `InvocationCounter`

--- shared/src/commonTest/kotlin/com/example/stories/testUtil/InvocationCounter.kt
+++ shared/src/commonTest/kotlin/com/example/stories/testUtil/InvocationCounter.kt
@@ -17,9 +17,77 @@ class InvocationCounter(val invocationsTarget: Int) {
         }
     }
 
+    inline fun <T> count(block: () -> T): T {
+        invoke()
+        return block()
+    }
+
     inline fun <T> use(block: (InvocationCounter) -> T): T {
         val result = block(this)
         verifyInvocations()
         return result
     }
 }
+
+inline fun<T> use(
+    c0: InvocationCounter,
+    c1: InvocationCounter,
+    block: (InvocationCounter, InvocationCounter) -> T
+): T {
+    val result = block(c0, c1)
+    c0.verifyInvocations()
+    c1.verifyInvocations()
+    return result
+}
+
+inline fun<T> use(
+    c0: InvocationCounter,
+    c1: InvocationCounter,
+    c2: InvocationCounter,
+    block: (InvocationCounter, InvocationCounter, InvocationCounter) -> T
+): T {
+    val result = block(c0, c1, c2)
+    c0.verifyInvocations()
+    c1.verifyInvocations()
+    c2.verifyInvocations()
+    return result
+}
+
+inline fun<T> use(
+    c0: InvocationCounter,
+    c1: InvocationCounter,
+    c2: InvocationCounter,
+    c3: InvocationCounter,
+    block: (InvocationCounter, InvocationCounter, InvocationCounter, InvocationCounter) -> T
+): T {
+    val result = block(c0, c1, c2, c3)
+    c0.verifyInvocations()
+    c1.verifyInvocations()
+    c2.verifyInvocations()
+    c3.verifyInvocations()
+    return result
+}
+
+inline fun<T> use(
+    c0: InvocationCounter,
+    c1: InvocationCounter,
+    c2: InvocationCounter,
+    c3: InvocationCounter,
+    c4: InvocationCounter,
+    block: (InvocationCounter, InvocationCounter, InvocationCounter, InvocationCounter, InvocationCounter) -> T
+): T {
+    val result = block(c0, c1, c2, c3, c4)
+    c0.verifyInvocations()
+    c1.verifyInvocations()
+    c2.verifyInvocations()
+    c3.verifyInvocations()
+    c4.verifyInvocations()
+    return result
+}
+
+inline fun<T> use(counters: List<InvocationCounter>, block: (List<InvocationCounter>) -> T): T {
+    val result = block(counters)
+    counters.forEach(InvocationCounter::verifyInvocations)
+    return result
+}
+
